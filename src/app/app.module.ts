2c39d6e9da90e1c0f5bce15f04bf2493efe03496
BaptisteBourlet <baptiste.bourlet@gmail.com>
2020-02-27 14:31:00 +0100

    debug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,12 +18,9 @@ import { InfoComponent } from './core/component/info/info.component';
 import { AdminUsersComponent } from './core/component/admin-users/admin-users.component';
 import { ProfilComponent } from './core/component/profil/profil.component';
 import { ActivitiesComponent } from './core/component/activities/activities.component';
-<<<<<<< HEAD
 import { CreateUserComponent } from './core/component/modal/create-user/create-user.component';
-=======
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
->>>>>>> dev
 
 @NgModule({
   declarations: [
