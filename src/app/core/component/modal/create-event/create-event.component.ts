ec897d7f9acfc917279c24de676583b39d40a67b
alexandre-becquart <becquart.alexandre@hotmail.fr>
2020-03-12 16:28:36 +0100

    CSS POPUP home page

--- src/app/core/component/modal/create-event/create-event.component.ts
+++ src/app/core/component/modal/create-event/create-event.component.ts
@@ -1,68 +1,99 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import {format, formatISO, addHours} from 'date-fns';
+import {
+  Component,
+  Inject,
+  OnInit
+} from '@angular/core';
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from '@angular/material/dialog';
+import {
+  format,
+  formatISO,
+  addHours
+} from 'date-fns';
+import {
+  fr
+} from 'date-fns/locale';
+
+import {
+  faCaretSquareLeft
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
   styleUrls: ['../modal-scss/style.scss']
-}) 
+})
 
 
 export class CreateEventComponent implements OnInit {
+
+  faCaretSquareLeft = faCaretSquareLeft
   constructor(
     public dialogRef: MatDialogRef<CreateEventComponent>,
     @Inject(MAT_DIALOG_DATA) public data) { }
 
   onNoClick(): void {
     this.dialogRef.close();
-
-    
   }
   viewDate;
-  view  : number;
+  view: number;
   res = {};
-  activityFound : string;
+  activityFound: string;
   taskFound: string;
   ngOnInit() {
-    
+
     if (this.data.taskId !== null) {
-      this.view= 2;
+      this.view = 2;
       this.res['taskId'] = parseInt(this.data.taskId);
+      this.next(null)
     } else {
       this.view = 1;
     }
-    
-    this.viewDate = format(this.data.date,'dd/MM/yy')
-  }
+    let day = this.upFirsChar(format(this.data.date, 'iiii', {
+      locale: fr
+    }));
+    this.viewDate = `${this.upFirsChar(day)}  ${format(this.data.date, 'dd')} ${this.upFirsChar(format(this.data.date, 'MMMM', { locale: fr }))} ${format(this.data.date, 'yyyy')}`;
 
+    // this.viewDate = format(this.data.date,'dd/MM/yy')
+  }
+  upFirsChar(d) {
+    return d.charAt(0).toUpperCase() + d.slice(1);
+  }
   next(f) {
-    this.view=2;
-    this.data.activities.find(act => {
-      return act['tasks'].some(tsk => tsk.id == f.value.tasks)
-    });
-    this.activityFound = this.data.activities.find(act =>  
-      act['tasks'].some(t => 
-        t.taskId === parseInt(f.value.tasks)
+    this.view = 2;
+    let taskId;
+    if (f !== null) {
+      console.log('true')
+      taskId = parseInt(f.value.tasks);
+    } else {
+      console.log("false")
+      taskId = parseInt(this.data.taskId);
+    }
+    this.activityFound = this.data.activities.find(act =>
+      act['tasks'].some(t =>
+        t.taskId === taskId
       )
     );
-    this.taskFound = this.activityFound['tasks'].find(tsk => 
-      tsk.taskId === parseInt(f.value.tasks)
+    console.log(this.activityFound)
+    this.taskFound = this.activityFound['tasks'].find(tsk =>
+      tsk.taskId === taskId
     );
-   
-    let taskId = parseInt(f.value.tasks) ;
     this.res['taskId'] = taskId;
   }
   submit(f) {
     // this.res['color'] = this.data.activities.find(e => e['tasks'].some(e => e.taskId === this.res['taskId'])).color_code;
     let start, end, description;
-    ({ start, end, description } = f.value)
+    ({
+      start,
+      end,
+      description
+    } = f.value)
     this.res['description'] = description;
     let date = this.data.date
-    this.res['start'] = formatISO(addHours(new Date((date).getFullYear(), (date).getMonth(), (date).getDate(), start.split(':')[0], start.split(':')[1]),1));
-    
+    this.res['start'] = formatISO(addHours(new Date((date).getFullYear(), (date).getMonth(), (date).getDate(), start.split(':')[0], start.split(':')[1]), 1));
     this.res['end'] = formatISO(addHours(new Date((date).getFullYear(), (date).getMonth(), (date).getDate(), end.split(':')[0], end.split(':')[1]), 1));
-
     this.dialogRef.close(this.res)
   }
 }
