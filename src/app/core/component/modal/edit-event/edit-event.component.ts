65ab203eeb0eb320b8dd4ed5d5880eb92037f8d4
alexandre-becquart <becquart.alexandre@hotmail.fr>
2020-03-13 15:17:45 +0100

    UPDATE CSS PopUp - Home page

--- src/app/core/component/modal/edit-event/edit-event.component.ts
+++ src/app/core/component/modal/edit-event/edit-event.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
-import { format , formatISO, addHours} from 'date-fns';
-interface event  {
+import { format, formatISO, addHours } from 'date-fns';
+import { fr } from 'date-fns/locale';
+interface event {
   start: string,
   end: string,
   meta: Object,
@@ -17,27 +18,36 @@ interface event  {
 export class EditEventComponent implements OnInit {
 
   constructor(public dialogRef: MatDialogRef<EditEventComponent>,
-    @Inject(MAT_DIALOG_DATA) public data) {}
-  
-  event : event;
-  
+    @Inject(MAT_DIALOG_DATA) public data) { }
+
+  event: event;
+  viewDate
+  date: Date;
   startText: string;
   endText: string;
   res = {};
   // end = format(event['end'], this.hourForm);
   ngOnInit() {
-    this.event= this.data.event;
+    this.event = this.data.event;
 
     let hourForm = 'HH:mm';
+    this.date = new Date(this.event.start);
     this.startText = format(new Date(this.event.start), hourForm);
     this.endText = format(new Date(this.event.end), hourForm);
+    let day = this.upFirsChar(format(this.date, 'iiii', {
+      locale: fr
+    }));
+    this.viewDate = `${this.upFirsChar(day)}  ${format(this.date, 'dd')} ${this.upFirsChar(format(this.date, 'MMMM', { locale: fr }))} ${format(this.date, 'yyyy')}`;
+  }
+  upFirsChar(d) {
+    return d.charAt(0).toUpperCase() + d.slice(1);
   }
   submit(f) {
     let start, end, description;
-    ({start, end, description} = f.value)
+    ({ start, end, description } = f.value)
     let date = new Date(this.event.start);
 
-    this.res['start'] = formatISO(addHours(new Date((date).getFullYear(), (date).getMonth(), (date).getDate(), start.split(':')[0], start.split(':')[1]),1));
+    this.res['start'] = formatISO(addHours(new Date((date).getFullYear(), (date).getMonth(), (date).getDate(), start.split(':')[0], start.split(':')[1]), 1));
     this.res['end'] = formatISO(addHours(new Date((date).getFullYear(), (date).getMonth(), (date).getDate(), end.split(':')[0], end.split(':')[1]), 1));
     this.res['description'] = description;
     // this.res['taskId'] = this.event.meta['taskId'];
--- src/app/core/component/modal/view-event/view-event.component.ts
+++ src/app/core/component/modal/view-event/view-event.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA, MatDialog, MatDialogConfig} from '@angular/material/dialog';
-import {format} from 'date-fns';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { format } from 'date-fns';
 import { DataService } from 'src/app/core/services/data.service';
 import { RemoveEventComponent } from '../remove-event/remove-event.component';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { fr } from 'date-fns/locale';
+
 
 @Component({
   selector: 'app-view-event',
@@ -10,6 +15,12 @@ import { RemoveEventComponent } from '../remove-event/remove-event.component';
   styleUrls: ['../modal-scss/style.scss']
 })
 export class ViewEventComponent implements OnInit {
+
+  faEdit = faEdit;
+  faTrash = faTrash;
+  faTimes = faTimes
+
+
   startHour;
   endHour;
   color;
@@ -18,20 +29,30 @@ export class ViewEventComponent implements OnInit {
   takName;
   event;
   date;
+  viewDate
   constructor(
     public dialogRef: MatDialogRef<ViewEventComponent>,
-    @Inject(MAT_DIALOG_DATA) public data, private DataService : DataService, private dialog: MatDialog) {}
+    @Inject(MAT_DIALOG_DATA) public data, private DataService: DataService, private dialog: MatDialog) { }
 
   ngOnInit() {
     let event = this.data.event;
-    this.startHour = format( event.start, 'HH:mm'); 
-    this.endHour =  format(event.end, 'HH:mm');
-    this.date = format(event.start, 'dd/MM/yyyy')
+    this.startHour = format(event.start, 'HH:mm');
+    this.endHour = format(event.end, 'HH:mm');
+    this.date = new Date(event.start);
     this.takName = event.meta.taskName;
     this.activityName = event.meta.activityName;
-    
+
     this.color = this.data.event.color.secondary;
+
+    let day = this.upFirsChar(format(this.date, 'iiii', {
+      locale: fr
+    }));
+    this.viewDate = `${this.upFirsChar(day)}  ${format(this.date, 'dd')} ${this.upFirsChar(format(this.date, 'MMMM', { locale: fr }))} ${format(this.date, 'yyyy')}`;
     // this.hidden = true;
+  }
+
+  upFirsChar(d) {
+    return d.charAt(0).toUpperCase() + d.slice(1);
   }
   onNoClick(): void {
     this.dialogRef.close();
@@ -48,8 +69,6 @@ export class ViewEventComponent implements OnInit {
     dialogRef.afterClosed().subscribe(res => {
       if (res === true) {
         this.dialogRef.close("removed");
-      } else {
-        this.dialogRef.close();
       }
     })
   }
