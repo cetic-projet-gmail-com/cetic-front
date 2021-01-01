d7d219962ffbe7d27e820f7c6530b9682cdbb339
ElianC <54775334+ElianC@users.noreply.github.com>
2021-01-01 19:03:21 +0100

    creating home/components/home-aside

---
+++ src/app/home/components/home-aside/home-aside.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-aside',
+  templateUrl: './home-aside.component.html',
+  styleUrls: ['./home-aside.component.scss'],
+})
+export class HomeAsideComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
