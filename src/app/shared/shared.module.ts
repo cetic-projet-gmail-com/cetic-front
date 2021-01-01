933e985d4bd1b2b61c2b36f4edf4623c8c8ab03e
ElianC <54775334+ElianC@users.noreply.github.com>
2021-01-01 16:38:14 +0100

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
 import { MaterialModule } from '../material.module';
 import { RouterModule } from '@angular/router';
 
