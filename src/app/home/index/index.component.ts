34c1ea457bf3d78967b0de247fb8ca286622ce94
ElianC <54775334+ElianC@users.noreply.github.com>
2021-01-01 19:02:52 +0100

    home wip

--- src/app/home/index/index.component.ts
+++ src/app/home/index/index.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss']
+  styleUrls: ['./index.component.scss'],
 })
 export class IndexComponent implements OnInit {
+  viewDate: Date = new Date();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
