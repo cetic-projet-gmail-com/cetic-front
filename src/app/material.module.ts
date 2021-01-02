9e76258c3daf8e4a8c43e9c0cbc52065013e795b
ElianC <54775334+ElianC@users.noreply.github.com>
2021-01-02 19:51:29 +0100

    Update material.module.ts

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, MatProgressBarModule, MatIconModule, MatMenuModule],
-  exports: [MatIconModule, MatMenuModule],
+  exports: [
+    MatIconModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+  ],
 })
 export class MaterialModule {}
