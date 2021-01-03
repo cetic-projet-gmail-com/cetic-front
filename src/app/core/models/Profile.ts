12d25913872c8bb2a52edd8b5956ff38fb63f1ab
ElianC <54775334+ElianC@users.noreply.github.com>
2021-01-03 17:20:20 +0100

    Create Profile.ts

---
+++ src/app/core/models/Profile.ts
@@ -0,0 +1,15 @@
+export interface Profile {
+  id: number;
+
+  email: string;
+  firstName: string;
+  lastName: string;
+}
+
+export interface ProfileForm {
+  id: number;
+
+  email: string;
+  firstName: string;
+  lastName: string;
+}
