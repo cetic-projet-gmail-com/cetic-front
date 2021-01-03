e8ebcccfb3081dc6d01079eb0b65cc8ba33ba041
ElianC <54775334+ElianC@users.noreply.github.com>
2021-01-03 17:20:23 +0100

    Update Event.ts

--- src/app/core/models/Event.ts
+++ src/app/core/models/Event.ts
@@ -1,8 +1,14 @@
-export default interface Event {
+export interface Event {
   id: number;
 
   description: string;
 
   startAt: string;
   endAt: string;
+}
+export interface EventForm {
+  id: number;
+  title: string;
+  start: Date;
+  end: Date;
 }
