a518fb389283079389c5d220851640e9bda36777
ElianC <54775334+ElianC@users.noreply.github.com>
2021-01-04 22:20:27 +0100

    Update admin.module.ts

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 import { IndexComponent } from './index/index.component';
 import { AdminRoutingModule } from './admin-routing.module';
-import { SharedModule } from '../shared/shared.module';
+import { UsersComponent } from './tabs/users/users.component';
+import { ActivitiesComponent } from './tabs/activities/activities.component';
+import { DepartmentsComponent } from './tabs/departments/departments.component';
+import { EditComponent as EditUserComponent } from './users/edit/edit.component';
 
 @NgModule({
-  declarations: [IndexComponent],
+  declarations: [
+    IndexComponent,
+    UsersComponent,
+    ActivitiesComponent,
+    DepartmentsComponent,
+    EditUserComponent,
+  ],
   imports: [AdminRoutingModule, CommonModule, SharedModule],
 })
 export class AdminModule {}
