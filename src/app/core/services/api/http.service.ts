51de7f83b2bd290a5fbec9b6ff9061c49d900fce
ElianC <54775334+ElianC@users.noreply.github.com>
2021-01-04 22:20:54 +0100

    wip

--- src/app/core/services/api/http.service.ts
+++ src/app/core/services/api/http.service.ts
@@ -11,40 +11,49 @@ export class HttpService {
 
   private token = { Authorization: 'Bearer ' + this.auth.getRawToken() };
 
-  private request(method: string, path: string, payload?: object) {
+  private request(
+    method: string,
+    path: string,
+    payload?: object,
+    header?: object
+  ) {
     const defaultPath = `${environment.apiUrl}/${path}`;
 
-    const header = {
+    const reqHeader = {
       headers: {
         ...this.token,
-        ...payload,
+        ...header,
       },
     };
 
-    switch (method) {
-      case 'get':
-        return this.http.get(defaultPath, header);
-      case 'delete':
-        return this.http.delete(defaultPath, header);
-      case 'patch':
-        return this.http.patch(defaultPath, header);
-      case 'post':
-        return this.http.post(defaultPath, header);
-      case 'put':
-        return this.http.put(defaultPath, header);
-      default:
-        throw new Error();
+    try {
+      switch (method) {
+        case 'get':
+          return this.http.get(defaultPath, reqHeader);
+        case 'delete':
+          return this.http.delete(defaultPath, reqHeader);
+        case 'patch':
+          return this.http.patch(defaultPath, payload, reqHeader);
+        case 'post':
+          return this.http.post(defaultPath, payload, reqHeader);
+        case 'put':
+          return this.http.put(defaultPath, payload, reqHeader);
+        default:
+          throw new Error();
+      }
+    } catch (error) {
+      throw new Error();
     }
   }
 
-  public delete = (path: string, payload?: object) =>
-    this.request('delete', path, payload);
-  public get = (path: string, payload?: object) =>
-    this.request('get', path, payload);
-  public patch = (path: string, payload?: object) =>
-    this.request('patch', path, payload);
-  public post = (path: string, payload?: object) =>
-    this.request('post', path, payload);
-  public put = (path: string, payload?: object) =>
-    this.request('put', path, payload);
+  public delete = (path: string, header?: object) =>
+    this.request('delete', path, {}, header);
+  public get = (path: string, header?: object) =>
+    this.request('get', path, {}, header);
+  public patch = (path: string, payload?: object, header?: object) =>
+    this.request('patch', path, payload, header);
+  public post = (path: string, payload?: object, header?: object) =>
+    this.request('post', path, payload, header);
+  public put = (path: string, payload: object, header?: object) =>
+    this.request('put', path, payload, header);
 }
