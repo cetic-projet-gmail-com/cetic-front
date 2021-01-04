16f1e48db4faaccdee23d3bd0d0e79e2f657ac93
ElianC <54775334+ElianC@users.noreply.github.com>
2021-01-04 19:41:01 +0100

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
-import { MaterialModule } from '../material.module';
+import { MaterialModule } from './material.module';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [HeaderComponent, NavComponent],
-  imports: [CommonModule, MaterialModule, RouterModule],
-  exports: [HeaderComponent],
+  imports: [CommonModule, MaterialModule, RouterModule, ReactiveFormsModule],
+  exports: [HeaderComponent, MaterialModule, ReactiveFormsModule],
 })
 export class SharedModule {}
